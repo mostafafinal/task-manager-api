ca1373500076041696655880a5abda5538ecb9bd
mostafafinal <mostafafinal55@gmail.com>
2025-03-02 16:56:01 +0200

    chore: update authentication controller register test to expect user data

--- __tests__/controllers/authController.test.ts
+++ __tests__/controllers/authController.test.ts
@@ -1,12 +1,19 @@
 import request from "supertest";
 import app from "../../src/index";
 import { faker } from "@faker-js/faker"
-import { closeDBForTesting, connectDBForTesting } from "../prePostTesting";
+import { closeDBForTesting } from "../prePostTesting";
 import { IUser } from "../../src/interfaces/schemas";
+import { User } from "../../src/models/User";
 
 describe("Auth Controller Test", () => {
   let user: IUser;
-  
+
+  afterAll(async () => {
+    await User.collection.drop();
+
+    await closeDBForTesting();
+  })
+
   beforeEach(() => {
     user = {
       firstName: faker.person.firstName(),
@@ -25,5 +32,6 @@ describe("Auth Controller Test", () => {
 
       expect(res.statusCode).toEqual(200);
       expect(res.body.status).toBe("success");
+      expect(res.body.user).toMatchObject<IUser>({...user, password: res.body.user.password})
   })
 });
