10d99162f354b636fdb386f7f63e3b8311256efb
mostafafinal <mostafafinal55@gmail.com>
2025-03-05 23:06:14 +0200

    feat: use passport custom stretegy to add jose strategy instead of standard jwt

--- src/configs/passport.ts
+++ src/configs/passport.ts
@@ -1,47 +1,73 @@
 import passport from "passport";
-import { IStrategyOptions, Strategy as LocalStrategy, VerifyFunction } from "passport-local";
-import { Strategy as JwtStrategy, ExtractJwt, VerifyCallback, StrategyOptions } from "passport-jwt";
-import { User } from "../models/User";
+import {
+  IStrategyOptions,
+  Strategy as LocalStrategy,
+  VerifyFunction,
+} from "passport-local";
+import passportCustom, { VerifiedCallback } from "passport-custom";
 import { loginUser } from "../services/authService";
+import { Request } from "express";
+import { jwtVerify } from "jose";
+import { User } from "../models/User";
 
 const localOpts: IStrategyOptions = {
-    usernameField: "email",
-}
-
-const verifyUserCredientials: VerifyFunction = async (username, password, done) => {
-    try {
-        const user = await loginUser({email: username, password: password});
-
-        if (!user) {
-            return done(null, false, { message: "Incorrect email or password" });
-        }
-
-        return done(null, user);
-    } catch (err) {
-        console.error(err);
-    }
+  usernameField: "email",
 };
 
-const localStrategy: LocalStrategy = new LocalStrategy(localOpts, verifyUserCredientials);
+const verifyUserCredientials: VerifyFunction = async (
+  username,
+  password,
+  done
+) => {
+  try {
+    const user = await loginUser({ email: username, password: password });
 
-const jwtOpts: StrategyOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET as string,
-}
-
-const verifyUserFromToken: VerifyCallback = async (payload, done) => {
-    try {
-        const user = await User.findOne(payload.sub);
-
-        if(!user) return done(null, false);
-
-        return done(null, user)
-    } catch (error) {
-        return done(error, false);
+    if (!user) {
+      return done(null, false, { message: "Incorrect email or password" });
     }
-}
 
-const jwtStrategy: JwtStrategy = new JwtStrategy(jwtOpts, verifyUserFromToken);
+    return done(null, user);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const localStrategy: LocalStrategy = new LocalStrategy(
+  localOpts,
+  verifyUserCredientials
+);
+
+const CustomStrategy = passportCustom.Strategy;
+
+const getTokenFromHeader = (request: Request): string | null => {
+  const authHeader = request.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer "))
+    return authHeader.split(" ")[1];
+
+  return null;
+};
+
+const joseVerifyCallback: VerifiedCallback = async (request, done) => {
+  try {
+    const token = getTokenFromHeader(request);
+
+    if (!token) return done(new Error("Missing token"), false);
+
+    const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
+    const { payload } = await jwtVerify(token, secretKey);
+
+    const user = await User.findOne({ email: payload.email });
+
+    if (!user) return done(null, false);
+
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+};
+
+const JoseStrategy = new CustomStrategy(joseVerifyCallback);
 
 passport.use(localStrategy);
-passport.use(jwtStrategy);
+passport.use("jose", JoseStrategy);
