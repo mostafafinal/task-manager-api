e9ceb5bb5c87268199998c5151f76ff6aa86a2b5
mostafafinal <mostafafinal55@gmail.com>
2025-03-05 23:07:27 +0200

    feat: add jose middleware to sign user token

--- src/interfaces/expressMiddleware.ts
+++ src/interfaces/expressMiddleware.ts
@@ -1,4 +1,17 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
+import { IUser } from "./schemas";
 
-export type RegularMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
 
+export type RegularMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
