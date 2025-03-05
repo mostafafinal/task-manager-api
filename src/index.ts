824f06fbeee8da4fae42c41092a1c17e7b12c1c7
mostafafinal <mostafafinal55@gmail.com>
2025-03-06 00:18:40 +0200

    refactor: update app entry point file initially

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,42 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import * as authController from "./controllers/authController";
+import { signToken } from "./middlewares/jose";
+import passport from "passport";
+import "./configs/passport";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DB = process.env.DB_URL as string;
+const DB = process.env.MONGO_URL as string;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use(urlencoded({ extended: false }));
+
 // Root endpoint
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+
+app.post("/user/register", authController.signUp);
+app.post(
+  "/user/login",
+  passport.authenticate("local", { session: false }),
+  signToken
+);
+app.get(
+  "/protected",
+  passport.authenticate("jose", { session: false }),
+  (req, res) => {
+    res.json({ message: "authorized" });
+  }
+);
 
 // Error handling middleware
 app.use(
@@ -25,12 +45,12 @@ app.use(
     req: express.Request,
     res: express.Response,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    next: express.NextFunction,
+    next: express.NextFunction
   ) => {
     console.error(err.stack);
 
     res.status(500).json({ error: "Something broke!" });
-  },
+  }
 );
 
 mongoose
