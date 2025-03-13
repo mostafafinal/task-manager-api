4c3507be0f455c72f6cff626e5a436b960c1174e
mostafafinal <mostafafinal55@gmail.com>
2025-03-13 08:53:28 +0200

    feat: configure Agenda for background jobs, and db offloading

---
+++ src/configs/agenda.ts
@@ -0,0 +1,19 @@
+import { Agenda } from "@hokify/agenda";
+
+const connectionOpts = {
+  db: { address: process.env.MONGO_URL as string, collection: "agendaJobs" },
+};
+
+const agenda: Agenda = new Agenda(connectionOpts);
+
+const jobTypes: string[] = process.env.JOB_TYPES
+  ? process.env.JOB_TYPES.split(",")
+  : [];
+
+jobTypes.forEach((type) => import("./jobs/" + type).then((job) => job(agenda)));
+
+if (jobTypes.length) {
+  agenda.start();
+}
+
+export default agenda;
