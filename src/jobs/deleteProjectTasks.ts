94d34676eaa039d3c671ff1f077e923282803a16
mostafafinal <mostafafinal55@gmail.com>
2025-03-13 10:36:25 +0200

    feat: add delete project tasks background job

---
+++ src/jobs/deleteProjectTasks.ts
@@ -0,0 +1,17 @@
+import { Agenda, Job } from "@hokify/agenda";
+import { Task } from "../models/Task";
+import { ObjectId } from "mongoose";
+
+export const deleteProjectTasks = (agenda: Agenda) => {
+  try {
+    if (!agenda) throw new Error("delete project tasks job error");
+
+    agenda.define("delete project tasks", async (job: Job) => {
+      const projectId = job.attrs.data as ObjectId;
+
+      await Task.deleteMany(projectId);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
