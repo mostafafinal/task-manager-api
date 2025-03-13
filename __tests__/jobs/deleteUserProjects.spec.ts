15969a5e2e5b401763a9fe16106516283fa1b858
mostafafinal <mostafafinal55@gmail.com>
2025-03-13 09:00:50 +0200

    test: add delete user projects job unit

---
+++ __tests__/jobs/deleteUserProjects.spec.ts
@@ -0,0 +1,34 @@
+import { deleteUserProjects } from "../../src/jobs/deleteUserProjects";
+import { Agenda, Job } from "@hokify/agenda";
+import { Project } from "../../src/models/Project";
+
+jest.mock("../../src/models/Project");
+
+afterAll(() => jest.clearAllMocks());
+
+const agendaMock = {
+  define: jest.fn(),
+} as unknown as jest.Mocked<Agenda>;
+const jobDataMock = {
+  attrs: {
+    data: {
+      userId: "id-mock",
+    },
+  },
+} as Job;
+
+test("delete user projects job test", async () => {
+  deleteUserProjects(agendaMock);
+
+  const jobCallbackMock = agendaMock.define.mock.calls[0][1];
+
+  Project.deleteMany = jest.fn();
+
+  await jobCallbackMock(jobDataMock);
+
+  expect(agendaMock.define).toHaveBeenCalledWith(
+    "delete user projects",
+    expect.any(Function)
+  );
+  expect(Project.deleteMany).toHaveBeenCalledWith({ userId: "id-mock" });
+});
