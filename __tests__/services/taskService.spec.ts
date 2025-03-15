07df1c7d862619053586541939e45fb4e5c44c6e
mostafafinal <mostafafinal55@gmail.com>
2025-03-15 04:15:55 +0200

    test: add task service units

---
+++ __tests__/services/taskService.spec.ts
@@ -0,0 +1,66 @@
+import {
+  createTask,
+  deleteTask,
+  getTasks,
+  updateTask,
+} from "../../src/services/taskService";
+import { Task } from "../../src/models/Task";
+import { faker } from "@faker-js/faker";
+import { TaskModel } from "../../src/types/schemas";
+import { Types } from "mongoose";
+
+jest.mock("../../src/models/Task");
+
+describe("task service testing", () => {
+  const id: Types.ObjectId = new Types.ObjectId(
+    faker.database.mongodbObjectId()
+  );
+  const task: TaskModel = {
+    name: faker.commerce.productName(),
+    deadline: faker.date.soon(),
+    status: faker.helpers.arrayElement(["active", "completed"]),
+    priority: faker.helpers.arrayElement(["low", "moderate", "high"]),
+    description: faker.commerce.productDescription(),
+    projectId: new Types.ObjectId(faker.database.mongodbObjectId()),
+  };
+
+  test("create new task test", async () => {
+    Task.create = jest.fn();
+
+    await createTask(task);
+
+    expect(Task.create).toHaveBeenCalledWith(task);
+  });
+
+  test("get all tasks test", async () => {
+    Task.find = jest.fn();
+
+    await getTasks(id);
+
+    expect(Task.find).toHaveBeenCalledWith({ projectId: id });
+  });
+
+  test("update task test", async () => {
+    const newData: Partial<TaskModel> = {
+      name: faker.commerce.productName(),
+      deadline: faker.date.future(),
+    };
+
+    Task.updateOne = jest.fn();
+
+    await updateTask(id, newData);
+
+    expect(Task.updateOne).toHaveBeenLastCalledWith(
+      { _id: id },
+      { ...newData }
+    );
+  });
+
+  test("delete task test", async () => {
+    Task.deleteOne = jest.fn();
+
+    await deleteTask(id);
+
+    expect(Task.deleteOne).toHaveBeenCalledWith({ _id: id });
+  });
+});
