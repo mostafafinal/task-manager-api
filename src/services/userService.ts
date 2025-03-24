5e7c65c82902cc1884779030942f20a912dea950
mostafafinal <mostafafinal55@gmail.com>
2025-03-24 17:37:45 +0200

    feat: add get user by id user service function

---
+++ src/services/userService.ts
@@ -0,0 +1,19 @@
+import { Types } from "mongoose";
+import { IUser } from "../types/schemas";
+import { User } from "../models/User";
+
+export const getUserById = async (
+  userId: Types.ObjectId
+): Promise<Partial<IUser> | undefined> => {
+  try {
+    if (!userId) throw Error("service: user id's not provided");
+
+    const user: Partial<IUser> | null = await User.findById({ id: userId });
+
+    if (!user) throw Error("service: user's not found");
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+};
