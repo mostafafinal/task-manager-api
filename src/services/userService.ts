6674d869a5a696d51685d2be0388d40d487c9e61
mostafafinal <mostafafinal55@gmail.com>
2025-03-26 06:00:12 +0200

    feat: add change password user service function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,7 @@
 import { Types } from "mongoose";
 import { IUser } from "../types/schemas";
 import { User } from "../models/User";
+import * as bcrypt from "../utils/bcryption";
 
 export const getUserById = async (
   userId: Types.ObjectId
@@ -15,6 +16,32 @@ export const getUserById = async (
     if (!user) throw Error("service: user's not found");
 
     return user;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const changeUserPassword = async (
+  userId: Types.ObjectId,
+  oldPassword: string,
+  newPassword: string
+): Promise<void | undefined> => {
+  try {
+    if (!userId || !oldPassword || !newPassword)
+      throw new Error("either user id or old/new password is not provided");
+
+    const userPassword: Partial<IUser> | null =
+      await User.findById(userId).select("password");
+
+    if (!(await bcrypt.verifyPassword(oldPassword, userPassword as string)))
+      throw new Error("current password's incorrect!");
+
+    const newHashedPassword: string = await bcrypt.hashPassword(newPassword);
+
+    if (await bcrypt.verifyPassword(newPassword, userPassword as string))
+      throw new Error("user password's already the same!");
+
+    await User.updateOne({ password: newHashedPassword });
   } catch (error) {
     console.error(error);
   }
