6be152f760f742a702ce588286efa0e2b0613678
mostafafinal <mostafafinal55@gmail.com>
2025-03-30 20:50:09 +0200

    test: add change password user service unit

--- __tests__/controllers/userController.spec.ts
+++ __tests__/controllers/userController.spec.ts
@@ -25,6 +25,18 @@ describe("User controller suite", () => {
     expect(res.body.user).toBeDefined();
   });
 
+  test("change user password request", async () => {
+    (service.changeUserPassword as jest.Mock) = jest.fn();
+
+    const res: Response = await request(app)
+      .put("/user/changepassword")
+      .send({ oldPassword: "12345678", newPassword: "123456789" })
+      .set("Cookie", `x-auth-token=${process.env.JWT_SIGNED_TOKEN}`);
+
+    expect(service.changeUserPassword).toHaveBeenCalled();
+    expect(res.statusCode).toEqual(201);
+  });
+
   test("delete user by id request", async () => {
     (service.deleteUserById as jest.Mock) = jest.fn();
 
