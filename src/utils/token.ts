ec00c7df0e3cda7f41cae5e989a82a06e38786c0
mostafafinal <mostafafinal55@gmail.com>
2025-03-31 13:27:18 +0200

    feat: add token util for signing & verifying JWTs

---
+++ src/utils/token.ts
@@ -0,0 +1,33 @@
+import { JwtPayload, Secret, sign, verify } from "jsonwebtoken";
+
+export const generateToken = async (payload: JwtPayload, secret: Secret) => {
+  try {
+    if (!payload || Object.keys(payload).length === 0 || !secret)
+      throw new Error("invalid payload or secret");
+
+    const token: string = sign(payload, secret, {
+      algorithm: "HS256",
+      expiresIn: "5m",
+    });
+
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const verifyToken = async (token: string, secret: Secret) => {
+  try {
+    if (!token || !secret) throw new Error("invalid token or secret");
+
+    const validToken = verify(token, secret, (err, payload) => {
+      if (err) return false;
+
+      return payload;
+    });
+
+    return validToken;
+  } catch (error) {
+    console.error(error);
+  }
+};
