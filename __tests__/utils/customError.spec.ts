1db1721d75e60d0a928b9cba8b77af8306f12457
mostafafinal <mostafafinal55@gmail.com>
2025-04-04 21:06:19 +0200

    test: add custom error util unit

---
+++ __tests__/utils/customError.spec.ts
@@ -0,0 +1,19 @@
+import { customError, CustomError } from "../../src/utils/customError";
+
+test("Custom error", async () => {
+  const errorMock: Partial<CustomError> = {
+    statusCode: 400,
+    message: "invalid data",
+    data: null,
+    details: null,
+  };
+
+  const error: CustomError = customError(
+    errorMock.statusCode,
+    errorMock.message,
+    errorMock.data,
+    errorMock.details
+  );
+
+  expect(error).toMatchObject<Partial<CustomError>>(errorMock);
+});
