6e3b4c1a90b68c7aae2a6c6156327519c9698b82
mostafafinal <mostafafinal55@gmail.com>
2025-04-05 02:07:39 +0200

    refactor: adjust authentication controller & router to efficiently handle and catch errors

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,55 +1,49 @@
 import * as authService from "../services/authService";
-import { RegularMiddleware } from "../types/expressMiddleware";
+import {
+  RegularMiddleware,
+  RegularMiddlewareWithoutNext,
+} from "../types/expressMiddleware";
 import passport, { AuthenticateCallback } from "passport";
-import { signToken } from "../middlewares/jwt";
 import { IUser } from "../types/schemas";
 import { NextFunction, Request, Response } from "express";
 import { HydratedDocument } from "mongoose";
-import isAuth from "../middlewares/isAuth";
+import { customError } from "../utils/customError";
 
-export const signUp: RegularMiddleware = async (req, res, next) => {
-  try {
-    const data: IUser = req.body;
+export const signUp: RegularMiddlewareWithoutNext = async (req, res) => {
+  const data: IUser = req.body;
 
-    await authService.registerUser({ ...data });
+  if (Object.keys(data).length >= 0)
+    throw customError("fail", 400, "invalid user data");
 
-    res.status(201).json({
-      status: "success",
-      message: "registered successfully!",
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).json({ status: "fail", message: error.message });
+  const result = await authService.registerUser({ ...data });
 
-      return;
-    }
+  if (!result) throw customError("fail", 500, "failed to register!");
 
-    next(error);
-  }
+  res.status(201).json({
+    status: "success",
+    message: "registered successfully!",
+  });
 };
 
-export const loginLocal = [
-  (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate("local", { session: false }, function (
-      err,
-      user,
-      info
-    ) {
-      if (err) {
-        return next(err);
-      }
+export const loginLocal = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("local", { session: false }, function (
+    err,
+    user,
+    info
+  ) {
+    if (err) {
+      return next(err);
+    }
 
-      if (!user) {
-        return res.status(401).json({ status: "fail", ...(info as object) });
-      }
+    if (!user) {
+      return res.status(401).json({ status: "fail", ...(info as object) });
+    }
 
-      req.user = user as HydratedDocument<IUser>;
+    req.user = user as HydratedDocument<IUser>;
 
-      next();
-    } as AuthenticateCallback)(req, res, next);
-  },
-  signToken,
-];
+    next();
+  } as AuthenticateCallback)(req, res, next);
+};
 
 export const loginGoogle: RegularMiddleware[] = [
   passport.authenticate("google", {
@@ -58,47 +52,33 @@ export const loginGoogle: RegularMiddleware[] = [
   }),
 ];
 
-export const loginGoogleCB: RegularMiddleware[] = [
-  passport.authenticate("google", {
+export const loginGoogleCB: RegularMiddleware = passport.authenticate(
+  "google",
+  {
     failureRedirect: "/",
     session: false,
-  }),
-  signToken,
-];
-
-export const forgetPasswordPost: RegularMiddleware = async (req, res, next) => {
-  try {
-    if (!req.body.email) throw new Error("user email's not provided");
-
-    await authService.forgetPassword(req.body.email);
-
-    res.status(200).json({
-      status: "success",
-      message: "we've sent a reset password email to you!",
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).json({ status: "fail", message: error.message });
-
-      return;
-    }
-
-    next(error);
   }
+);
+
+export const forgetPasswordPost: RegularMiddlewareWithoutNext = async (
+  req,
+  res
+) => {
+  if (!req.body.email) throw customError("fail", 400, "invalid user email");
+
+  await authService.forgetPassword(req.body.email);
+
+  res.status(200).json({
+    status: "success",
+    message: "we've sent a reset password email to you!",
+  });
 };
 
-export const logout: [RegularMiddleware[], RegularMiddleware] = [
-  isAuth,
-  async (req, res, next) => {
-    try {
-      req.user = {} as HydratedDocument<IUser>;
+export const logout: RegularMiddlewareWithoutNext = async (req, res) => {
+  req.user = {} as HydratedDocument<IUser>;
 
-      res
-        .status(204)
-        .clearCookie("x-auth-token", { maxAge: 0, sameSite: "strict" })
-        .end();
-    } catch (error) {
-      next(error);
-    }
-  },
-];
+  res
+    .status(204)
+    .clearCookie("x-auth-token", { maxAge: 0, sameSite: "strict" })
+    .end();
+};
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,18 +1,25 @@
 import { Router } from "express";
 import * as authController from "../controllers/authController";
+import isAuth from "../middlewares/isAuth";
+import { tryCatch } from "../utils/tryCatch";
+import { signToken } from "../middlewares/jwt";
 
 const authRouter: Router = Router();
 
-authRouter.post("/register", authController.signUp);
+authRouter.post("/register", tryCatch(authController.signUp));
 
-authRouter.post("/login", authController.loginLocal);
+authRouter.post("/login", authController.loginLocal, tryCatch(signToken));
 
 authRouter.get("/google", authController.loginGoogle);
 
-authRouter.get("/google/callback", authController.loginGoogleCB);
+authRouter.get(
+  "/google/callback",
+  authController.loginGoogleCB,
+  tryCatch(signToken)
+);
 
 authRouter.post("/forgetpassword", authController.forgetPasswordPost);
 
-authRouter.delete("/logout", ...authController.logout);
+authRouter.delete("/logout", isAuth, tryCatch(authController.logout));
 
 export default authRouter;
