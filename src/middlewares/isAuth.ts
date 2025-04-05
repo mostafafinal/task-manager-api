7ed349de7761f9c451bc46cdb58a5abae19d0f40
mostafafinal <mostafafinal55@gmail.com>
2025-04-05 05:27:26 +0200

    refactor: adjust authentication checker middleware to handle error efficiently

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,9 +1,14 @@
 import passport from "passport";
 import { RegularMiddleware } from "../types/expressMiddleware";
+import { customError } from "../utils/customError";
 
 const isAuth: RegularMiddleware[] = [
   (req, res, next) => {
+    if (!req.cookies["x-auth-token"])
+      throw next(customError("fail", 401, "you're not authorized!"));
+
     req.headers.authorization = "Bearer " + req.cookies["x-auth-token"];
+
     next();
   },
   passport.authenticate("jwt", { session: false }),
