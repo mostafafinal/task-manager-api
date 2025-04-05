6d592de30ae02250f3fd9f1af5c2d3139c1d66ed
mostafafinal <mostafafinal55@gmail.com>
2025-04-05 02:06:42 +0200

    refactor: adjust sign token middleware to handle error efficiently

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -1,33 +1,32 @@
 import { JwtPayload, sign, SignOptions } from "jsonwebtoken";
-import { RegularMiddleware } from "../types/expressMiddleware";
+import { RegularMiddlewareWithoutNext } from "../types/expressMiddleware";
+import { customError } from "../utils/customError";
 
-export const signToken: RegularMiddleware = async (req, res, next) => {
-  try {
-    if (!req.user) throw new Error("user data're not found");
+export const signToken: RegularMiddlewareWithoutNext = async (req, res) => {
+  if (!req.user || Object.keys(req.user).length <= 0) throw customError("fail");
 
-    const payload: JwtPayload = {
-      id: req.user.id as string,
-    };
+  const payload: JwtPayload = {
+    id: req.user.id as string,
+  };
 
-    const secret = process.env.JWT_SECRET as string;
+  const secret = process.env.JWT_SECRET as string;
 
-    const options: SignOptions = {
-      algorithm: "HS256",
-      expiresIn: "1d",
-    };
+  const options: SignOptions = {
+    algorithm: "HS256",
+    expiresIn: "1d",
+  };
 
-    const token = sign(payload, Buffer.from(secret), options);
+  const token = sign(payload, Buffer.from(secret), options);
 
-    res.cookie("x-auth-token", token, {
-      maxAge: 1000 * 60 * 60 * 24 * 3,
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-      partitioned: true,
-    });
+  if (!token) throw customError("fail");
 
-    res.json({ status: "success", message: "successfully logged" });
-  } catch (err) {
-    next(err);
-  }
+  res.cookie("x-auth-token", token, {
+    maxAge: 1000 * 60 * 60 * 24 * 3,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    partitioned: true,
+  });
+
+  res.json({ status: "success", message: "successfully logged" });
 };
