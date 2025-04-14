5cb45d7fd648e3d35b458c3083d905f9a189e4c4
mostafafinal <mostafafinal55@gmail.com>
2025-04-14 22:28:03 +0200

    docs: document custom error utility for further clarity

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -1,9 +1,36 @@
+/**
+ * @file customError.ts
+ * @author Mostafa Hasan (mostafafinal55@gmail.com)
+ * @summary
+ *  This file declares customError util
+ *  it's responsible for handling any http errors and send a JSON HTTP
+ *  customized error response e.g.:
+ *  { status: fail, statusCode: 500, data: internal server error, details: ..etc}
+ * @version 1.0.0
+ * @date 2025-04-04
+ * @copyright Copyrights (c) 2025
+ */
+
 export interface CustomError extends Error {
   status: string;
   statusCode: number;
   data: unknown;
   details: unknown;
 }
+
+/**
+ * @description
+ *  CustomError inherits Error Constractuor and adds
+ *  custom error properties for it. finallay it returns
+ *  the customized error to be used in multiple cases
+ *  e.g. router handlers, global handlers.
+ * @param status
+ * @param statusCode
+ * @param message
+ * @param data
+ * @param details
+ * @returns { status: fail, statusCode: 500, data: internal server error, details: ..etc} JSON HTTP Error
+ */
 
 export const customError = (
   status: string = "fail",
