35eb86e4951d7155e03dc8e66a4322d45ffa82df
mostafafinal <mostafafinal55@gmail.com>
2025-04-17 00:47:32 +0200

    test: add get projects progress internal service util unit

--- __tests__/services/insightsService.spec.ts
+++ __tests__/services/insightsService.spec.ts
@@ -1,7 +1,10 @@
 import * as util from "../../src/utils/countModelFields";
 import * as service from "../../src/services/insightsService";
 import { faker } from "@faker-js/faker";
+import { prisma } from "../../src/configs/prisma";
+import { projects } from "../../src/types/prisma";
 
+const prismaMock = jest.mocked(prisma);
 jest.mock("../../src/utils/countModelFields");
 
 afterEach(() => jest.clearAllMocks());
@@ -21,6 +24,30 @@ describe("insight service suite", () => {
     status: statusMock,
     priority: priorityMock,
   };
+  const project = {
+    id: faker.database.mongodbObjectId(),
+    name: faker.commerce.productName(),
+    priority: faker.helpers.arrayElement(["low, moderate, high"]),
+    status: faker.helpers.arrayElement(["active", "completed"]),
+    tasks: [1, 2, 3, 4, 5], //simulating completed tasks
+    _count: { tasks: 10 },
+  } as unknown as projects;
+  const progressInsightsMock: service.ProgjectProgressModel[] = [
+    {
+      id: project.id,
+      name: project.name,
+      priority: project.priority,
+      status: project.status,
+      progress: 50,
+    },
+    {
+      id: project.id,
+      name: project.name,
+      priority: project.priority,
+      status: project.status,
+      progress: 50,
+    },
+  ];
 
   test("general info internal service util", async () => {
     jest
@@ -44,14 +71,30 @@ describe("insight service suite", () => {
     });
   });
 
+  test("projects progress intenral service util", async () => {
+    prismaMock.projects.findMany.mockResolvedValue([project, project]);
+
+    const insights = await service.getProjectsProgress(idMock);
+
+    expect(prismaMock.projects.findMany).toHaveBeenCalled();
+    expect(insights).toMatchObject<service.ProgjectProgressModel[]>(
+      progressInsightsMock
+    );
+  });
+
   test("projects insight service", async () => {
     jest.spyOn(service, "getGeneralInfo").mockResolvedValue(generalInfoMock);
+    jest
+      .spyOn(service, "getProjectsProgress")
+      .mockResolvedValue(progressInsightsMock);
 
     const insights = await service.projectsInsight(idMock);
 
     expect(service.getGeneralInfo).toHaveBeenCalledWith(idMock, "projects");
-    expect(insights).toMatchObject<service.TasksInsightModel>({
+    expect(service.getProjectsProgress).toHaveBeenCalledWith(idMock);
+    expect(insights).toMatchObject<service.ProjectsInsightModel>({
       general: generalInfoMock,
+      progresses: progressInsightsMock,
     });
   });
 
