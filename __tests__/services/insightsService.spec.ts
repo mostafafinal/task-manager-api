61c5e9579ddfc8358746bd46363a511213b2288f
mostafafinal <mostafafinal55@gmail.com>
2025-04-16 03:30:53 +0200

    test: add insights service projects insight unit

---
+++ __tests__/services/insightsService.spec.ts
@@ -0,0 +1,41 @@
+import * as util from "../../src/utils/countModelFields";
+import * as service from "../../src/services/insightsService";
+import { faker } from "@faker-js/faker";
+
+jest.mock("../../src/utils/countModelFields");
+
+afterEach(() => jest.clearAllMocks());
+
+describe("insight service suite", () => {
+  const statusMock = {
+    "0": { _count: 10, status: "completed" },
+    "1": { _count: 10, status: "active" },
+  };
+  const priorityMock = {
+    "0": { _count: 10, priority: "modetate" },
+    "1": { _count: 10, priority: "high" },
+  };
+  const idMock = faker.database.mongodbObjectId();
+
+  jest
+    .spyOn(util, "countModelFields")
+    .mockResolvedValueOnce(statusMock)
+    .mockResolvedValueOnce(priorityMock);
+
+  test("project insights service", async () => {
+    const insights = await service.projectsInsight(idMock);
+
+    expect(util.countModelFields).toHaveBeenCalledTimes(2);
+    expect(util.countModelFields).toHaveBeenCalledWith(idMock, "projects", [
+      "status",
+    ]);
+    expect(util.countModelFields).toHaveBeenLastCalledWith(idMock, "projects", [
+      "priority",
+    ]);
+    expect(insights).toMatchObject({
+      total: 20,
+      status: statusMock,
+      priority: priorityMock,
+    });
+  });
+});
