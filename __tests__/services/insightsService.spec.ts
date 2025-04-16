072876a693a844627b4fd8b97750fcfd5dc02e96
mostafafinal <mostafafinal55@gmail.com>
2025-04-16 17:44:40 +0200

    refactor: separate insightsService duplicate login into an interal service util

--- __tests__/services/insightsService.spec.ts
+++ __tests__/services/insightsService.spec.ts
@@ -7,6 +7,7 @@ jest.mock("../../src/utils/countModelFields");
 afterEach(() => jest.clearAllMocks());
 
 describe("insight service suite", () => {
+  const idMock = faker.database.mongodbObjectId();
   const statusMock = {
     "0": { _count: 10, status: "completed" },
     "1": { _count: 10, status: "active" },
@@ -15,15 +16,19 @@ describe("insight service suite", () => {
     "0": { _count: 10, priority: "modetate" },
     "1": { _count: 10, priority: "high" },
   };
-  const idMock = faker.database.mongodbObjectId();
+  const generalInfoMock: service.GeneralInfo = {
+    total: 20,
+    status: statusMock,
+    priority: priorityMock,
+  };
 
-  jest
-    .spyOn(util, "countModelFields")
-    .mockResolvedValueOnce(statusMock)
-    .mockResolvedValueOnce(priorityMock);
+  test("general info internal service util", async () => {
+    jest
+      .spyOn(util, "countModelFields")
+      .mockResolvedValueOnce(statusMock)
+      .mockResolvedValueOnce(priorityMock);
 
-  test.skip("project insights service", async () => {
-    const insights = await service.projectsInsights(idMock);
+    const insights = await service.getGeneralInfo(idMock, "projects");
 
     expect(util.countModelFields).toHaveBeenCalledTimes(2);
     expect(util.countModelFields).toHaveBeenCalledWith(idMock, "projects", [
@@ -32,27 +37,32 @@ describe("insight service suite", () => {
     expect(util.countModelFields).toHaveBeenLastCalledWith(idMock, "projects", [
       "priority",
     ]);
-    expect(insights).toMatchObject({
+    expect(insights).toMatchObject<service.GeneralInfo>({
       total: 20,
       status: statusMock,
       priority: priorityMock,
     });
   });
 
-  test("task insights service", async () => {
+  test("projects insight service", async () => {
+    jest.spyOn(service, "getGeneralInfo").mockResolvedValue(generalInfoMock);
+
+    const insights = await service.projectsInsight(idMock);
+
+    expect(service.getGeneralInfo).toHaveBeenCalledWith(idMock, "projects");
+    expect(insights).toMatchObject<service.TasksInsightModel>({
+      general: generalInfoMock,
+    });
+  });
+
+  test("tasks insight service", async () => {
+    jest.spyOn(service, "getGeneralInfo").mockResolvedValue(generalInfoMock);
+
     const insights = await service.tasksInsights(idMock);
 
-    expect(util.countModelFields).toHaveBeenCalledTimes(2);
-    expect(util.countModelFields).toHaveBeenCalledWith(idMock, "tasks", [
-      "status",
-    ]);
-    expect(util.countModelFields).toHaveBeenLastCalledWith(idMock, "tasks", [
-      "priority",
-    ]);
-    expect(insights).toMatchObject({
-      total: 20,
-      status: statusMock,
-      priority: priorityMock,
+    expect(service.getGeneralInfo).toHaveBeenCalledWith(idMock, "tasks");
+    expect(insights).toMatchObject<service.TasksInsightModel>({
+      general: generalInfoMock,
     });
   });
 });
--- src/services/insightsService.ts
+++ src/services/insightsService.ts
@@ -1,4 +1,4 @@
-/** * @file projectService.ts
+/** * @file insightsService.ts
  * @author Mostafa Hasan // (mostafafinal55@gmail.com)
  * @summary
  *  This file declares a combination of insight services
@@ -9,30 +9,41 @@
  * @copyright Copyrights (c) 2025
  */
 
+import { Prisma } from "../types/prisma";
 import { countModelFields } from "../utils/countModelFields";
 import { logger } from "../utils/logger";
 
-export type ProjectsInsights = (userId: string) => Promise<unknown>;
+export interface GeneralInfo {
+  total: number;
+  status: object;
+  priority: object;
+}
+
+export type GetGeneralInfo = (
+  userId: string,
+  modelName: Prisma.TypeMap["meta"]["modelProps"]
+) => Promise<GeneralInfo | undefined>;
 
 /**
  * @description
- *  ProjectsInsights service provides projects related insights
- *  e.g. total projects, projects' status & priorities for a
- *  specific user. Use cases e.g. dashboards, and reports
+ *  GetGeneralInfo internal service util provides projects & tasks related insights
+ *  e.g. total projects/tasks, projects/tasks' status & priorities for a
+ *  specific user. Is used by projectsInsights & tasksInsights services
  *
  * @function countModelFields
  *  util would be used for handling
- *  and retrieving projects' insights
+ *  and retrieving projects & tasks' insights
  *
- * @param userId to retrieve projects' insights for a specific
+ * @param userId to retrieve projects & tasks' insights for a specific
  *  user
- * @return projects' insights analytics
- * @example projectsInsights("user-id") // {total: n, status: {objs}, priority: {objs}}
+ * @return projects & tasks' insights analytics
+ * @example getGeneralInfo("user-id", "projects") // {total: n, status: {objs}, priority: {objs}}
  */
 
-export const projectsInsights: ProjectsInsights = async (userId) => {
+export const getGeneralInfo: GetGeneralInfo = async (userId, modelName) => {
   try {
     if (!userId || userId.length !== 24) throw new Error("invalid user id");
+    if (!(modelName in Prisma.ModelName)) throw new Error("invalid model name");
 
     const statusInsights = await countModelFields(userId, "projects", [
       "status",
@@ -42,66 +53,98 @@ export const projectsInsights: ProjectsInsights = async (userId) => {
     ]);
 
     if (!statusInsights || !priorityInsights)
-      throw new Error("failed to get projects insights");
+      throw new Error("failed to get general info insights");
 
-    const totalProjects = Object.keys(statusInsights).reduce(
+    const totalInsight = Object.keys(statusInsights).reduce(
       // @ts-expect-error validated and tested util
       (acc, curr) => acc + statusInsights[curr]._count,
       0
     );
 
     return {
-      total: totalProjects,
+      total: totalInsight,
       status: statusInsights,
       priority: priorityInsights,
     };
   } catch (error) {
-    logger.error(error, "PROJECTS INSIGHTS SERVICE EXCEPTION");
+    logger.error(error, "GENERAL INFO INTERNAL SERVICE UTIL EXCEPTION");
   }
 };
 
-export type TasksInsights = (userId: string) => Promise<unknown>;
+export interface ProjectsInsightModel {
+  general: GeneralInfo | undefined;
+}
+
+export type ProjectsInsight = (
+  userId: string
+) => Promise<ProjectsInsightModel | undefined>;
 
 /**
  * @description
- *  TasksInsights service provides tasks related insights
- *  e.g. total tasks, tasks' status & priorities for a
+ *  ProjectsInsight service provides projects related insight
+ *  e.g. projects general info, projects' progress for a
  *  specific user. Use cases e.g. dashboards, and reports
  *
- * @function countModelFields
- *  util would be used for handling
- *  and retrieving tasks' insights
+ * @function getGeneralInfo
+ *  internal service util would be used for handling
+ *  and retrieving projects' general information
+ * @function getProgresses
+ *  internal service util would be used for handling
+ *  and retrieving each project progress
+ *
+ * @param userId to retrieve projects' insights for a specific
+ *  user
+ * @return projects' insights analytics
+ * @example projectsInsights("user-id") // {general: GeneralInfo}
+ */
+
+export const projectsInsight: ProjectsInsight = async (userId) => {
+  try {
+    if (!userId || userId.length !== 24) throw new Error("invalid user id");
+
+    const info = await getGeneralInfo(userId, "projects");
+
+    return { general: info };
+  } catch (error) {
+    logger.error(error, "PROJECTS INSIGHT SERVICE EXCEPTION");
+  }
+};
+
+export interface TasksInsightModel {
+  general: GeneralInfo | undefined;
+}
+
+export type TasksInsights = (
+  userId: string
+) => Promise<TasksInsightModel | undefined>;
+
+/**
+ * @description
+ *  TasksInsight service provides tasks related insight
+ *  e.g. tasks general info, tasks' progress for a
+ *  specific user. Use cases e.g. dashboards, and reports
+ *
+ * @function getGeneralInfo
+ *  internal service util would be used for handling
+ *  and retrieving tasks' general information
+ * @function getProgresses
+ *  internal service util would be used for handling
+ *  and retrieving each project progress
  *
  * @param userId to retrieve tasks' insights for a specific
  *  user
  * @return tasks' insights analytics
- * @example tasksInsights("user-id") // {total: n, status: {objs}, priority: {objs}}
+ * @example tasksInsights("user-id") // {general: GeneralInfo}
  */
 
 export const tasksInsights: TasksInsights = async (userId) => {
   try {
     if (!userId || userId.length !== 24) throw new Error("invalid user id");
 
-    const statusInsights = await countModelFields(userId, "tasks", ["status"]);
-    const priorityInsights = await countModelFields(userId, "tasks", [
-      "priority",
-    ]);
+    const info = await getGeneralInfo(userId, "tasks");
 
-    if (!statusInsights || !priorityInsights)
-      throw new Error("failed to get tasks insights");
-
-    const totalTasks = Object.keys(statusInsights).reduce(
-      // @ts-expect-error validated and tested util
-      (acc, curr) => acc + statusInsights[curr]._count,
-      0
-    );
-
-    return {
-      total: totalTasks,
-      status: statusInsights,
-      priority: priorityInsights,
-    };
+    return { general: info };
   } catch (error) {
-    logger.error(error, "TASKS INSIGHTS SERVICE EXCEPTION");
+    logger.error(error, "TASKS INSIGHT SERVICE EXCEPTION");
   }
 };
